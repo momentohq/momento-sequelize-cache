d26ebbb2361278f3fccaa9f78d5ebab5f4410272
Pratik Agarwal <pratik@momentohq.com>
2023-10-09 12:52:10 -0700

    feat: add compression while interacting with Momento

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { MomentoClientGenerator } from './cacheclient/momento/momento-client-generator';
 export { LoggerFactory } from './logger/logger-factory'
-export { modelCacheFactory } from './model-cache-factory'
+export { modelCacheFactory, CompressionType, ModelCacheOptions } from './model-cache-factory'
 
 
